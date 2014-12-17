4980898f5e23f28dc5dae267b9461b14ec5ef02f
satoko_s <b1412491@planet.kanazawa-it.ac.jp>
2014-12-17 18:33:28 +0900

    ピラミッド

--- test/main.cpp
+++ test/main.cpp
@@ -15,6 +15,19 @@ int main()
 	>> >> >> > 34f2404d8eef36f3f478841b7c944fe77faa9ae1
 
 		cout << "unagi!\n" << endl;
+
+	    int n = 5;
+
+    for(int i=1; i<=n; i++){
+        for(int x=n; x>=i; x--){
+            printf(" ");
+        }
+        for(int y=1; y<=2*i-1; y++){
+            printf("A");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
 
