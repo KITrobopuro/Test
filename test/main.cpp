07e3ededa5796e771c06f11ea83eddd51ed6862f
t-nagatani <b1400245@planet.kanazawa-it.ac.jp>
2014-12-17 18:47:39 +0900

    ?

--- test/main.cpp
+++ test/main.cpp
@@ -2,10 +2,14 @@
 
 using namespace std;
 
+<<<<<<< HEAD
 
 int addition(int a,int b){
     return a+b;
 }
+=======
+int subtraction(int a, int b);
+>>>>>>> c03e93300f70f3d94f864c68a3943e22d5a61be4
 
 int main()
 {
@@ -18,4 +22,14 @@ int main()
 >>>>>>> 34f2404d8eef36f3f478841b7c944fe77faa9ae1
     return 0;
 }
+<<<<<<< HEAD
+=======
+int subtraction(int a, int b)
+{
+    int result =0;
 
+    result = a - b;
+
+    return (result);
+}
+>>>>>>> c03e93300f70f3d94f864c68a3943e22d5a61be4
